23b07ebe8467c572e8c99a01fe5dfa10ac90f720
Francisco420 <60949682+Francisco420@users.noreply.github.com>
2020-02-29 16:34:08 -0600

    Add files via upload

---
+++ MvcDesarrollo/MvcDesarrollo/Controllers/VideoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+
+
+namespace MvcDesarrollo.Controllers
+{
+    public class VideoController : Controller
+    {
+        //
+        // GET: /Video/
+
+        public ActionResult Index()
+        {
+            //Consultar datos en la DB
+            ViewData["video"] = BaseHelper.ejecutarConsulta(
+                "SELECT * FROM video",
+                CommandType.Text);
+            return View();
+        }
+
+    }
+}
---
+++ MvcDesarrollo/MvcDesarrollo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcDesarrollo.MvcApplication" Language="C#" %>
